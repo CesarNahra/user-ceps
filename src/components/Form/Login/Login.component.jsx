d773643f7d7ca25591ea1854d6c58e105413721b
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-07-10 22:12:07 -0300

    fix branch

--- src/components/Form/Login/Login.component.jsx
+++ src/components/Form/Login/Login.component.jsx
@@ -1,7 +1,15 @@
 export const FormLoginComponent = () => {
     return(
-        <>
-            <p>FormLoginComponent is render</p>
-        </>
+        <form>
+            <div className="input-group">
+                <label htmlFor="email">E-mail</label>
+                <input type="text" name="email" id="email" placeholder="Digite o seu email"/>
+            </div>
+            <div className="input-group">
+                <label htmlFor="password">E-mail</label>
+                <input type="password" name="password" id="password" placeholder="Digite a sua senha"/>
+            </div>
+            <button type="submit">Logar</button>
+        </form>
     );
 }
--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -2,16 +2,8 @@ import { FormLoginComponent } from "../../components/Form/Login/Login.component"
 
 export const LoginPage = () => {
     return(
-        <form>
-            <div className="input-group">
-                <label htmlFor="email">E-mail</label>
-                <input type="text" name="email" id="email" placeholder="Digite o seu email"/>
-            </div>
-            <div className="input-group">
-                <label htmlFor="password">E-mail</label>
-                <input type="password" name="password" id="password" placeholder="Digite a sua senha"/>
-            </div>
-            <button type="submit">Logar</button>
-        </form>
+        <>
+            <FormLoginComponent/>
+        </>
     );
 }
