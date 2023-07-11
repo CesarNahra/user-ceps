9987948c7694fde776ad3f7fe1c39e05b62d216f
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-07-10 22:10:59 -0300

    ação de redirecionar para home

--- src/components/Form/Login/Login.component.jsx
+++ src/components/Form/Login/Login.component.jsx
@@ -1,7 +1,24 @@
 export const FormLoginComponent = () => {
-    return(
+
+    const navigate = useNavigate();
+
+    const redirectToLogin = () => {
+        navigate('/home')
+    }
+
+    return (
         <>
-            <p>FormLoginComponent is render</p>
+            <form onSubmit={redirectToLogin}>
+                <div className="input-group">
+                    <label htmlFor="email">E-mail</label>
+                    <input type="text" name="email" id="email" placeholder="Digite o seu email" />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">E-mail</label>
+                    <input type="password" name="password" id="password" placeholder="Digite a sua senha" />
+                </div>
+                <button type="submit">Logar</button>
+            </form>
         </>
     );
 }
