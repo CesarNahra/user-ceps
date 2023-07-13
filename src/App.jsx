620a9fa0a938589fee0246b5735ac744f8120b1f
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-07-13 20:04:31 -0300

    implementando auth user via context

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<LoginPage/>}/>
-        <Route path='/home' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/' element={<HomePage/>}/>
       </Routes>
     </Router>
   )
