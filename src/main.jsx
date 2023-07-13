75e8579b0373bcdd0d338d65399751d9f3152051
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-07-13 20:05:39 -0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import { AuthProvider } from './contexts/auth/auth.context.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle/>
-    
+
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,17 +1,17 @@
-import { useContext } from "react";
-import { AuthContext } from "../../contexts/auth/auth.context";
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/auth/auth.context"
 import { Navigate } from "react-router";
 
 export const HomePage = () => {
-    const { auth } = useContext(AuthContext);
+  const { auth } = useContext(AuthContext);
 
-    const render = () => {
-        return(
-            <>
-                <p>HomePage is render</p>
-            </>
-        );
-    }
-    
-    return auth.isLogged ? render() : <Navigate to='/login'/>
+  const render = () => {
+    return(
+      <>
+        <p>HomePage is Render</p>
+      </>
+    )
+  }
+
+  return auth.isLogged ? render() : <Navigate to='/login'/>
 }
